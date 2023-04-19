2bed3362977fb437678a3b2d04efb8a0d37ea9c8
Marco Arriaga <marriaga158@gmail.com>
2023-04-18 21:10:19 -0400

    more updates

--- main.R
+++ main.R
@@ -4,7 +4,17 @@ library("dplyr")
 # What we need to do:
 # Separate out the average wind speeds per month per station
 
-April22 <- read_excel("Wind Speed Data - April 2022 through March 2023.xlsx",sheet = "April 2022")
-April22Avgs <- April22 %>%
-  group_by(City) %>%
-  summarise(avg=mean(DailyWindMax))
+generate_avgs <- function(month_data){
+  month_data %>%
+    group_by(City) %>%
+    summarise(avg=mean(DailyWindSpeedAvg))
+}
+
+path <- "Wind Speed Data - April 2022 through March 2023.xlsx"
+sheetnames <- excel_sheets(path)
+monthSheets <- lapply(excel_sheets(path), read_excel, path = path)
+
+names(monthSheets) <- sheetnames
+# issue with name here
+April22 <- monthSheets['April 2022'][1]
+April22Avg <- generate_avgs(monthSheets['April 2022'])
