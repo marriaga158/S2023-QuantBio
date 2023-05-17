c553be76d83c5b7be2855acb74bcff801de61d8f
xaviles <55527999+xaviles@users.noreply.github.com>
2023-05-16 23:22:41 -0400

    add code for LaTeX output of monthly windspeed/windpower tables

--- main.R
+++ main.R
@@ -2,16 +2,20 @@ library("readxl")
 library("dplyr")
 library("ggplot2")
 
+library("knitr")
+library("kableExtra")
+library("magrittr")
+
 # What we need to do:
 # Separate out the average wind speeds per month per station
 
 generate_avgs <- function(month_data){
   temp <- month_data %>%
     group_by(City) %>%
-    summarise(num_measurements=n(),
+    summarise(n=n(),
               avg=mean(DailyWindSpeedAvg),
-              standard_dev=sd(DailyWindSpeedAvg, na.rm=TRUE))
-  temp$k <- with(temp, (standard_dev/avg) ^ -1.086)
+              sdev=sd(DailyWindSpeedAvg, na.rm=TRUE))
+  temp$k <- with(temp, (sdev/avg) ^ -1.086)
   #temp <- temp %>%
   #  rowwise() %>%
   #  mutate(a = gamma(k))
@@ -20,12 +24,12 @@ generate_avgs <- function(month_data){
   # I am not entirely confident that this is the correct way to do this
   # because plugging this into online calculators gives nonreal answers
   temp$a <- with(temp, avg/gamma(1+(1/k)) )
-  temp$avg_monthly_weibull_wind_speed <- with(temp, a*gamma(1+(1/k)) )
+  temp$avgWindSpeed <- with(temp, a*gamma(1+(1/k)) )
   # !!! THIS ASSUMES A 95% AIR DENSITY !!!
   # i couldn't get air density calculations so this is
   # what we got
-  temp$avg_monthly_weibull_power_wind_potential <-
-    with(temp, num_measurements * (0.5 * 0.95 * (avg^3) * gamma(1+3/k) ))
+  temp$avgPowWindPoten <-
+    with(temp, n * (0.5 * 0.95 * (avg^3) * gamma(1+3/k) ))
   temp
 }
 
@@ -64,3 +68,65 @@ March23Avg <-
 # the data
 ggplot(April22Avg, aes(x=City, y=avg)) + geom_bar(stat="identity") + 
   theme(axis.text.x = element_text(angle = 90))
+
+# All of these below output the LaTeX for the tables in the appendix of the report
+#April22
+April22Avg %>%
+  # mutate(across(where(is.numeric), round, digits = 1)) %>% ## this is deprecated, use
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "April 2022", booktabs = TRUE)
+
+#May22
+May22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "May 2022", booktabs = TRUE)
+
+#June22
+June22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "June 2022", booktabs = TRUE)
+
+#July22
+July22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "July 2022", booktabs = TRUE)
+
+#August22
+August22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "August 2022", booktabs = TRUE)
+
+#September22
+September22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "September 2022", booktabs = TRUE)
+
+#October22
+October22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "October 2022", booktabs = TRUE)
+
+#November22
+November22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "November 2022", booktabs = TRUE)
+
+#December22
+December22Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "December 2022", booktabs = TRUE)
+
+#January23
+January23Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "January 2023", booktabs = TRUE)
+
+#February23
+February23Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "February 2023", booktabs = TRUE)
+
+#March23
+March23Avg %>%
+  mutate(across(where(is.numeric), \(x) round(x, digits = 1))) %>%
+  kable(format = 'latex', caption = "March 2023", booktabs = TRUE)
